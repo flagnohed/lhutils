253fa2dc9aecf401935c1e3963501707fb7515a8
felix <felix@lagnohed.se>
2025-12-07 17:28:14 +0100

    added icetime distribution translation + structs

---
+++ c/game.h
@@ -0,0 +1,34 @@
+#ifndef GAME_H_
+#define GAME_H_
+
+#include <stdint.h>
+
+typedef enum {
+    EVENT_INV,
+    EVENT_GOAL,
+    EVENT_SHOT,
+    EVENT_PENALTY,
+    EVENT_INJURY,
+}   Event_t;
+
+/* Struct containing statistics for each player that participates
+ * in at least one event in a game. */
+typedef struct {
+    char name[256];
+    uint8_t shots;
+    uint8_t assists;
+    uint8_t penalties;
+    uint8_t goals_pp;
+    uint8_t goals_bp;
+    uint8_t goals_es;
+
+}   GamePlayer_t;
+
+typedef enum {
+    char name[256];  /* TODO: Check Livehockey max team name length. */
+    GamePlayer_t players[32];  /* TODO: 32 is probably overkill, maybe change? */
+    unsigned int icetime_distribution;  /* Percentages per line, e.g., 343432, 404020, ... */
+    unsigned int grade;
+}   Team_t;
+
+#endif
--- c/parser.c
+++ c/parser.c
@@ -145,6 +145,28 @@ int parse_transfer_list() {
     return 0;
 }
 
+/* Translates icetime_distribution to its corresponding icetime string (ABCABC...).
+ * The last 5 characters of the returned string are for sudden death. */
+const char *get_icetime_str(unsigned int icetime_distribution) {
+    switch (icetime_distribution) {
+        case 343432:
+            return "ABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCABCAB";
+        case 404020:
+            return "ABABCABABCABABCABABCABABCABABCABABCABABCABABCABABCABABCABABCABABC";
+        case 403525:
+            return "ABABCABABCACABCABABCABABCACABCABABCABABCACABCABABCABABCABABCABABC";
+        case 403030:
+            return "ABCABCABACABCABCABCAABCABCABACABCABCABCAABCABCABACABCABCABCAABCAB";
+        case 453520:
+            return "ABABCACABCABABCABABAABABCACABCABABCABABAABABCACABCABABCABABAABABC";
+        case 454015:
+            return "ABABCABABCABABCABABAABABCABABCABABCABABAABABCABABCABABCABABAABABC";
+        default:
+            printf("Invalid icetime distribution: %d\n", icetime_distribution);
+            return NULL;
+    }
+}
+
 int parse_game(void) {
     printf("parse_game()\n");
     return 0;
